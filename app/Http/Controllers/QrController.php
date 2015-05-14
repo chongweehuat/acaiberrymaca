83cdace55f935d962d844c6bb296348acd0e0c4f
chongwehuat <chongweehuat@gmail.com>
2015-05-14 15:24:36 +0800

    update background image

--- app/Http/Controllers/QrController.php
+++ app/Http/Controllers/QrController.php
@@ -8,21 +8,24 @@ class QrController extends Controller {
 	public function index($code)
 	{
 		$qrcodes=DB::table('qrcode')->where('code',$code)->get();
+		$aresult=array('msg'=>'','report'=>0);
 		if(isset($qrcodes[0])){
 			if($qrcodes[0]->last_checked=='0000-00-00 00:00:00'){
-				$msg="您目前手上的产品是合格正品，请放心食用。";
+				$aresult['msg']="您目前手上的产品是合格正品，请放心食用。";
 			}else{				
-				$msg="防伪二维码已在{$qrcodes[0]->last_checked}已验证过，<br>您目前手上的产品很可能是伪劣仿制品，<br>请勿食用，请前往客服部申报。";
+				$aresult['msg']="防伪二维码已在{$qrcodes[0]->last_checked}已验证过，<br>您目前手上的产品很可能是伪劣仿制品，<br>请勿食用，请前往客服部申报。";
+				$aresult['report']=1;
 			}
 			DB::table('qrcode')
 				->where('id',$qrcodes[0]->id)
 				->update(['last_checked'=>date('Y-m-d H:i:s'),'checked_count'=>$qrcodes[0]->checked_count+1]);
 		}else{
-			$msg="无效的防伪二维码，<br>您目前手上的产品很可能是伪劣仿制品，<br>请勿食用，请前往客服部申报。";
+			$aresult['msg']="无效的防伪二维码，<br>您目前手上的产品很可能是伪劣仿制品，<br>请勿食用，请前往客服部申报。";
+			$aresult['report']=1;
 		}
 
-		$msg='扫描验证结果:<br><br>'.$msg;
+		$aresult['msg']='扫描验证结果:<br><br>'.$aresult['msg'];
 
-		return view('welcome',compact("msg"));
+		return view('welcome',compact("aresult"));
 	}
 }

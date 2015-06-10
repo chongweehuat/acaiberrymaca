feeb430c87374bef4fc533973366da0c1d8e0e15
chongwehuat <chongweehuat@gmail.com>
2015-06-10 10:27:48 +0800

    change from phpstack to laravel

--- config/database.php
+++ config/database.php
@@ -55,9 +55,9 @@ return [
 		'mysql' => [
 			'driver'    => 'mysql',
 			'host'      => env('DB_HOST', 'localhost'),
-			'database'  => env('DB_DATABASE', 'azhyknqxbx'),
-			'username'  => env('DB_USERNAME', 'azhyknqxbx'),
-			'password'  => env('DB_PASSWORD', '6Z9RWk8Jxc'),
+			'database'  => env('DB_DATABASE', 'fmtxrxscbr'),
+			'username'  => env('DB_USERNAME', 'fmtxrxscbr'),
+			'password'  => env('DB_PASSWORD', 'QZ9NFbeBC8'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
